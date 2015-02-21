b39decd6e19f7e7fe6aa3ef3db310b827f22d891
Jason Sich <jasich@gmail.com>
2015-02-21 16:54:11 -0500

    WIP on MealOptionsStore + Flux

---
+++ src/scripts/constants/MealOptionsConstants.js
@@ -0,0 +1,7 @@
+const keyMirror = require('react/lib/keyMirror');
+
+module.exports = keyMirror({
+  MEAL_OPTIONS_FILTER_CHANGED: null,
+  MEAL_OPTIONS_MEAL_REMOVED: null,
+  MEAL_OPTIONS_MEAL_ADDED: null
+});
--- src/scripts/stores/MealOptionsStore.js
+++ src/scripts/stores/MealOptionsStore.js
@@ -1,14 +1,26 @@
-const React = require('react');
-const { List } = require('immutable');
+const React            = require('react');
+const { List }         = require('immutable');
+const { EventEmitter } = require('events');
 
 const data = require('json!../data.json');
+
+const AppDispatcher             = require('../AppDispatcher');
+const MealOptionsConstants      = require('../constants/MealOptionsConstants');
+
+
+const CHANGE_EVENT = 'meal-options-change';
 
 const _mealTimes = List(data.mealTimes);
 const _mealOptions = List(data.mealOptions);
 let _availableOptions;
 
 
-class MealOptionsStore {
+function filterOptions(filterValue) {
+  _availableOptions = _mealOptions.filter(x => { return x.name.indexOf(filterValue) === 0; });
+}
+
+
+class MealOptionsStore extends EventEmitter {
 
   constructor() {
     _availableOptions = List(_mealOptions);
@@ -21,7 +33,39 @@ class MealOptionsStore {
   getAvailableOptions() {
     return _availableOptions;
   }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
 }
 
+const _mealOptionsStore = new MealOptionsStore();
 
-module.exports = new MealOptionsStore();
+AppDispatcher.register(function(action) {
+    var change = true;
+
+    switch(action.actionType) {
+      case MealOptionsConstants.MEAL_OPTIONS_FILTER_CHANGED:
+          filterOptions(action.value);
+        break;
+
+      default:
+        change = false;
+        break;
+    }
+
+    if(change) {
+      _mealOptionsStore.emitChange();
+    }
+});
+
+
+module.exports = _mealOptionsStore;
